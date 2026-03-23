5a14a7d52ee649b217cbef002d183c7a90885727
djdjoko <djdjoko@users.noreply.github.com>
2026-03-23 17:49:44 +0100

    test: auth test stubs

---
+++ tests/test_auth.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def test_login_valid_credentials():
+    # TODO: implement with test DB
+    pass
+
+
+def test_login_invalid_credentials():
+    pass
+
+
+def test_refresh_token_rotation():
+    # Not implemented yet
+    pass
